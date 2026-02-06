252a4e952876ad11b0b5b684533dab910f1d01d1
plus <plu5@users.noreply.github.com>
2026-02-06 14:40:56 +0200

    feat(md): local variable always-update-modified-date-field

--- pm.el
+++ pm.el
@@ -309,6 +309,7 @@ replaces common OCR issues and also replaces breaklines with spaces"
   (let ((date (if from-disk (copy-file-last-date-to-clipboard nil t)
                 (format-time-string "%F %H:%M"))))
     (re-replace-text "^modified_date: .*$" (concat "modified_date: " date))))
+(set (make-local-variable 'always-update-modified-date-field) nil)
 (defun maybe-update-modified-date-field ()
   (interactive)
   ;; yibe https://stackoverflow.com/a/6886731/18396947
@@ -317,8 +318,13 @@ replaces common OCR issues and also replaces breaklines with spaces"
     (save-excursion
       (beginning-of-buffer)
       (when (and (re-search-forward "^modified_date:")
-                 (y-or-n-p "Update modified_date? "))
-        (update-modified-date-field nil)))))
+                 (or always-update-modified-date-field
+                     (y-or-n-p "Update modified_date? ")))
+        (update-modified-date-field nil)
+        (unless always-update-modified-date-field
+          (message
+           "setq always-update-modified-date-field t to remember this choice")
+          )))))
 (add-hook 'before-save-hook #'maybe-update-modified-date-field)
 
 ;;; FILE DEFUNS (not bound to anything atm, but useful functions to be able to run with M-x)
